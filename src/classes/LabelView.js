ecb8232acf9a1c7cbdd5ef36caac94f26758a4b3
Masamoto Miyata <miyata@sincere-co.com>
2015-05-05 16:40:37 +0900

    ラベルの横位置はrelativeでキープしwindowに対して縦にCSSで動かすように実装。

--- src/classes/LabelView.js
+++ src/classes/LabelView.js
@@ -12,8 +12,17 @@ Timeline.LabelView.prototype._getClassName = function(){
 };
 
 Timeline.LabelView.prototype._build = function(){
-
+  var self = this;
+  Timeline.window.on("scroll", function(){
+    self.top(Timeline.window.scrollTop());
+  });
 };
+
+Timeline.LabelView.prototype.top = function(top){
+  this._element.css({
+    "top": top + 'px'
+  });
+}
 
 Timeline.LabelView.prototype.addLabel = function(label){
   var self = this;
